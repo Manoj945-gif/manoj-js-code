fa2fe2e0bb5028ad91649db00d56d989d0016578
Manoj945-gif <manojsaw9128@gmail.com>
2025-06-10 06:19:42 +0000

    logicoperation

---
+++ basics/conversion.js
@@ -0,0 +1,36 @@
+console.log("conversion");
+ //Normal conversion
+
+// console.log(2+3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2-3);
+// console.log(2%3);
+
+
+console.log("1"+2+3);//6 123
+console.log(1+4+"2"+3);//9-10 523
+console.log(1+2+"3");//3-4 33
+
+console.log((2+3)+4*5);//5+4*5
+
+/*
+notes In this situation javascript work in left hand Side 
+  if firstly found string value then all value treat as a string 
+*/
+
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2;
+
+console.log(num2);
+
+
+let gameCounter=100;
+gameCounter++;
+
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
