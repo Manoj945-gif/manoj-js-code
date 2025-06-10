68cfd76595a60005c6f37cea335cfc1f3510240b
Manoj945-gif <manojsaw9128@gmail.com>
2025-06-10 05:40:32 +0000

    conversiondatatypes

---
+++ basics/datatypeConversion.js
@@ -0,0 +1,26 @@
+// console.log("conversion of dataTypes");
+
+let isLoggedNumber=33;
+
+console.log(typeof isLoggedNumber);
+
+let stringLoggedNumber=String(isLoggedNumber);
+
+console.log(stringLoggedNumber);
+
+console.log(typeof stringLoggedNumber);
+
+
+
+// conversion of string to boolean
+
+let customerName=1;
+console.log(customerName)
+
+let numberStringname=Boolean(customerName);
+
+console.log(numberStringname);
+console.log(typeof numberStringname);
+
+//we cant change string to number like"MANOJ KR"=>NAN give.
+
