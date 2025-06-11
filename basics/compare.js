fc3831dffd509671a542193b94cfa0158a932dc8
Manoj945-gif <manojsaw9128@gmail.com>
2025-06-11 06:35:12 +0000

    comparison of conditon

---
+++ basics/compare.js
@@ -0,0 +1,29 @@
+console.log("comparison of condition");
+
+// console.log(2>3);//false
+// console.log(2>=3);//false
+// console.log(2<3);//true
+// console.log(2<=3);//true
+// console.log(2==3);//false
+// console.log(2!=3);//true
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);//0>1 f
+// console.log(null<0);//0<1 f
+// console.log(null==0);//NAN==0
+// console.log(null>=0);//t
+
+
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined>=0);
+
+
+//=== and ==
+// check value and also check datatypes === but == only check value not a data types 
+
+console.log("2"==2);
+console.log("2"===2);
