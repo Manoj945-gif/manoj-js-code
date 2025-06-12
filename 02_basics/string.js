982a10e45dd437aeea29a5221a8b112f7ea2e0b4
Manoj945-gif <manojsaw9128@gmail.com>
2025-06-12 07:33:41 +0000

    string

---
+++ 02_basics/string.js
@@ -0,0 +1,23 @@
+console.log("Use of string ");
+
+/*  
+string defined krna ka do trika hai 
+
+
+*/
+ //first trika 
+let name="manoj kr saw";
+
+
+//second trika 
+let name2=new String ("manoj kr saw ");
+
+
+console.log(name.length);
+console.log(name2.length);
+
+// console.log(name.indexof('m'));
+console.log(name2.indexOf('m'));
+console.log(name2.charAt(3));
+
+
